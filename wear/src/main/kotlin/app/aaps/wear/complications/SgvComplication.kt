6faca894e1762b16d5ec36ec937a2cf3dec3f369
Pavel <pajin74@gmail.com>
2025-11-24 14:33:22 +0100

    Update SVG complication

--- wear/src/main/kotlin/app/aaps/wear/complications/SgvComplication.kt
+++ wear/src/main/kotlin/app/aaps/wear/complications/SgvComplication.kt
@@ -25,8 +25,9 @@ class SgvComplication : BaseComplicationProviderService() {
         when (dataType) {
             ComplicationData.TYPE_SHORT_TEXT -> {
                 val builder = ComplicationData.Builder(ComplicationData.TYPE_SHORT_TEXT)
-                    .setShortText(ComplicationText.plainText(raw.singleBg[0].sgvString + raw.singleBg[0].slopeArrow + "\uFE0E"))
-                    .setShortTitle(ComplicationText.plainText(displayFormat.shortTrend(raw, 0)))
+                    //.setShortText(ComplicationText.plainText(raw.singleBg[0].sgvString + raw.singleBg[0].slopeArrow + "\uFE0E"))
+                    .setShortText(ComplicationText.plainText(raw.singleBg[0].sgvString))
+                    .setShortTitle(ComplicationText.plainText(raw.singleBg[0].slopeArrow + "\uFE0E" + displayFormat.shortTrend(raw, 0)))
                     .setTapAction(complicationPendingIntent)
                 complicationData = builder.build()
             }
